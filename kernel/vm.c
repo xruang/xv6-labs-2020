269c88e474ef5202ee08ed3bf6eb819b27bae648
ceoi <xruang@163.com>
2026-05-18 15:39:33 +0800

    change

--- kernel/vm.c
+++ kernel/vm.c
@@ -139,7 +139,7 @@ int uvmshouldtouch(uint64 va) {
   struct proc *p = myproc();
   
   return va < p->sz // within size of memory for the process
-    && PGROUNDDOWN(va) != r_sp() // not accessing stack guard page (it shouldn't be mapped)
+    && va >= p->trapframe->sp // not accessing stack guard page (it shouldn't be mapped)
     && (((pte = walk(p->pagetable, va, 0))==0) || ((*pte & PTE_V)==0)); // page table entry does not exist
 }
 
